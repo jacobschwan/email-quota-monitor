919a824ff9779f8625dd863ac2d6757976326235
Jacob Schwan <blackbrass88@gmail.com>
2021-03-24 23:30:56 +0000

    setup infinite loop & limit messages

--- check_quota.R
+++ check_quota.R
@@ -10,35 +10,46 @@ url <- paste0("imaps://", user, ":", pass, "@", server)
 
 qc_handle <- curl::new_handle(customrequest = 'GETQUOTA ""')
 
-qc_response <- curl::curl_fetch_memory(url, qc_handle)
+previous_ratio <- 0
 
-quotas <- qc_response$header %>%
-    rawToChar() %>%
-    str_extract('(?<=QUOTA \\"\\" \\()(\\w+ \\d+ \\d+ *)+')
-
-qc_names <- quotas %>%
-    str_extract_all("[A-Z]+") %>%
-    unlist()
-
-qc_values <- quotas %>%
-    str_extract_all("\\d+ \\d+", simplify = T)  %>%
-    map(str_split, pattern = " ", simplify = T) %>%
-    map(as.numeric)
-
-names(qc_values) <- qc_names
-
-qc_ratio <- qc_values %>%
-    map(~.x[1]/.x[2])
-
-timestamp()
-print(qc_ratio)
-
-if(qc_ratio$STORAGE >= 0.995) {
-    po_title <- glue::glue("Email {scales::percent(qc_ratio$STORAGE, accuracy = .01)} Full!")
-    po_message <- glue::glue("Email is {scales::percent(qc_ratio$STORAGE, accuracy = .01)} full at {round(qc_values$STORAGE[1]/1024/1024, 3)} Gb")
+repeat {
+    timestamp()
+    qc_response <- curl::curl_fetch_memory(url, qc_handle)
     
-    pushover_emergency(title = po_title,
-                       message = po_message)
+    quotas <- qc_response$header %>%
+        rawToChar() %>%
+        str_extract('(?<=QUOTA \\"\\" \\()(\\w+ \\d+ \\d+ *)+')
+    
+    qc_names <- quotas %>%
+        str_extract_all("[A-Z]+") %>%
+        unlist()
+    
+    qc_values <- quotas %>%
+        str_extract_all("\\d+ \\d+", simplify = T)  %>%
+        map(str_split, pattern = " ", simplify = T) %>%
+        map(as.numeric)
+    
+    names(qc_values) <- qc_names
+    
+    qc_ratio <- qc_values %>%
+        map(~.x[1]/.x[2])
+    
+    print(qc_ratio)
+    print(map(qc_values, ~.x/1024/1024))
+    
+    if(qc_ratio$STORAGE >= 0.98 & qc_ratio$STORAGE != previous_ratio) {
+        po_title <- glue::glue("Email {scales::percent(qc_ratio$STORAGE, accuracy = .01)} Full!")
+        po_message <- glue::glue("Email is {scales::percent(qc_ratio$STORAGE, accuracy = .01)} full at {round(qc_values$STORAGE[1]/1024/1024, 3)} GB")
+        
+        pushover_emergency(title = po_title,
+                           message = po_message)
+    }
+    
+    previous_ratio <- qc_ratio$STORAGE
+    
+    Sys.sleep(300)
 }
+
+
 
 
